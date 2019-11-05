e312051e05a7e515ad9b9c76a353a65ab60c28fe
Liang-Xiaobin <1490556728@qq.com>
2019-11-05 15:07:25 +0800

    添加目录包结构

---
+++ src/main/java/cn/edu/gdupt/client/SocketClient.java
@@ -0,0 +1,14 @@
+package cn.edu.gdupt.client;
+
+/**
+ * socket通信客户端
+ *
+ * @author Liang Xiaobin<1490556728@qq.com>
+ * @version 2019/11/5
+ * @since JDK1.8
+ */
+public class SocketClient {
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ src/main/java/cn/edu/gdupt/server/SocketServer.java
@@ -0,0 +1,14 @@
+package cn.edu.gdupt.server;
+
+/**
+ * socket通信服务端
+ *
+ * @author Liang Xiaobin<1490556728@qq.com>
+ * @version 2019/11/5
+ * @since JDK1.8
+ */
+public class SocketServer {
+    public static void main(String[] args) {
+
+    }
+}
