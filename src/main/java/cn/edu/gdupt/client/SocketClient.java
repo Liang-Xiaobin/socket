a3ec28f93d37f299e5b33ff15b883e614cb08a53
Liang-Xiaobin <1490556728@qq.com>
2019-11-06 10:36:17 +0800

    完善socket通信的客户端与服务端

--- src/main/java/cn/edu/gdupt/client/SocketClient.java
+++ src/main/java/cn/edu/gdupt/client/SocketClient.java
@@ -1,5 +1,8 @@
 package cn.edu.gdupt.client;
 
+import java.io.*;
+import java.net.Socket;
+
 /**
  * socket通信客户端
  *
@@ -9,6 +12,36 @@ package cn.edu.gdupt.client;
  */
 public class SocketClient {
     public static void main(String[] args) {
-        
+        try {
+            //创建Socket对象
+            Socket socket = new Socket("106.54.211.175", 8888);
+            //根据输入输出流和服务端连接
+            //获取一个输出流
+            OutputStream outputStream = socket.getOutputStream();
+            //将输出流包装
+            PrintWriter printWriter = new PrintWriter(outputStream);
+            printWriter.print("服务端你好,我是balala");
+            printWriter.flush();
+            //关闭输出流
+            socket.shutdownOutput();
+            //获取一个输入流，接收服务端信息
+            InputStream inputStream = socket.getInputStream();
+            InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+            BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+            String info = "";
+            String temp = null;
+            while ((temp = bufferedReader.readLine()) != null) {
+                info += temp;
+                System.out.println("客户端接收服务端发送信息:" + info);
+            }
+            //关闭相对应的资源
+            bufferedReader.close();
+            inputStream.close();
+            printWriter.close();
+            outputStream.close();
+            socket.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
--- src/main/java/cn/edu/gdupt/server/SocketServer.java
+++ src/main/java/cn/edu/gdupt/server/SocketServer.java
@@ -1,5 +1,9 @@
 package cn.edu.gdupt.server;
 
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 /**
  * socket通信服务端
  *
@@ -9,6 +13,40 @@ package cn.edu.gdupt.server;
  */
 public class SocketServer {
     public static void main(String[] args) {
+        try {
+            ServerSocket serverSocket = new ServerSocket(8888);
+            System.out.println("服务端已经启动,等待客户端连接");
+            while (true) {
+                Socket socket = serverSocket.accept();
+                //根据输入输出流和客户端连接
+                InputStream inputStream = socket.getInputStream();
+                InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+                BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+                String temp = null;
+                String info = "";
+                while ((temp = bufferedReader.readLine()) != null) {
+                    info += temp;
+                    System.out.println("已接收到客户端连接");
+                    System.out.println("服务端接收到客户端信息:" + info + "当前客户端ip为:");
+                }
+                //获取一个输出流
+                OutputStream outputStream = socket.getOutputStream();
+                //将输出流包装
+                PrintWriter printWriter = new PrintWriter(outputStream);
+                printWriter.print("你好，服务端已经接收到你的信息");
+                printWriter.flush();
+                //关闭输出流
+                socket.shutdownOutput();
+                //关闭相对应资源
+                printWriter.close();
+                outputStream.close();
+                bufferedReader.close();
+                inputStream.close();
+                socket.close();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
     }
 }
